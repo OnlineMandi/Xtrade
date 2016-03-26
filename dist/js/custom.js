ad422d75f59f31ad0cbbc6475d6e4ad3f8ef17f6
gurwinder61ds <gurwinder.61ds@gmail.com>
2016-03-26 15:32:59 +0530

    index page

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -1,3 +1,16 @@
 /*
  grunt-getting-started 2016-02-25 
 */
+
+
+$(document).ready(function(){
+ var i = 1;
+ setInterval(function(){
+  i--;
+  var bgposi = i + "px center";
+
+  $(".footer-logolist").css({
+   backgroundPosition:bgposi,
+  });
+ }, 10);
+});
